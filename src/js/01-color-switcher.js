79ad15b25f629607411f11a83f504797e89349ac
LidiyaYem <102388444+LidiyaYem@users.noreply.github.com>
2023-03-08 17:01:25 +0200

    task-1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,33 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+};
+
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function onStart(e) {
+    
+    intervalId = setInterval(changeBgColor, 1000);
+    // e.currentTarget.disabled = true;
+    // refs.stopBtn.disabled = false;
+    refs.startBtn.toggleAttribute('disabled');
+}
+
+function onStop(e) {
+    
+    clearInterval(intervalId);
+    // e.currentTarget.disabled = true;
+    // refs.startBtn.disabled = false;
+    refs.startBtn.removeAttribute('disabled');
+}
+
+function changeBgColor() {
+    const hexColor = getRandomHexColor();
+    refs.body.style.backgroundColor = hexColor;
+}
